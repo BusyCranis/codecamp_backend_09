792d88ae1d584d371d7db21fa516a5b60b4bb113
cranis <cranis2@naver.com>
2022-09-03 19:51:02 +0900

    faefasef

--- homework/day05/frontend/menu/index.js
+++ homework/day05/frontend/menu/index.js
@@ -1,3 +1,77 @@
+
+// 커피 목록 조회 API를 요청해주세요.
+const getCoffee = () => {
+  // let payloads
+
+  console.log('index.js 파일의 openMenu 함수 안에서 getCoffee가 실행 됨')
+  // 1. 백엔드 서버로 /starbucks API 요청해 커피 데이터를 받는다.
+
+  axios.get("http://localhost:3000/starbucks").then((res) => {
+    console.log(res.data.posts);
+    // payloads = res.data.posts;
+    createMenuCard(res.data.posts)
+  }).catch((e) => {
+    console.error(e.message);
+  });
+
+  // 2. 받은 데이터로 createMenuCard 함수를 이용해 메뉴 카드를 모두 만들어주세요.
+  // createMenuCard(payloads)
+
+}
+
+const createMenuCard = (data) => {
+
+  for (var i = 0; i < data.length; i++) {
+
+    const menuCardWrapper = document.createElement('div')
+    menuCardWrapper.className = 'Menu_Card_Wrapper'
+
+    const menuCardImgBox = document.createElement('div')
+    menuCardImgBox.className = 'Menu_Card_ImgBox'
+
+    const menuCardName = document.createElement('div')
+    menuCardName.className = 'Menu_Card_Name'
+    menuCardName.textContent = data[i].name || '메뉴이름'
+
+    const menuCardInfo = document.createElement('div')
+    menuCardInfo.className = 'Menu_Card_Info'
+    menuCardInfo.textContent = data[i].kcal || '칼로리'
+
+    const menuWrapper = document.querySelector('#Menu_Background')
+    menuCardWrapper.appendChild(menuCardImgBox)
+    menuCardWrapper.appendChild(menuCardName)
+    menuCardWrapper.appendChild(menuCardInfo)
+    menuWrapper.appendChild(menuCardWrapper)
+
+  }
+
+
+
+  console.log(data)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const openMenu = () => {
   console.log("메뉴 열기 버튼 클릭")
   // 모달창 열기
